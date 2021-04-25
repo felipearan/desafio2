6f1216fe9285c2a28509d269c422a8586ceb6cc4
felipearan <felipe.arancibia.r@gmail.com>
2021-04-25 12:45:48 -0400

    desafio2

--- desafio-2/src/App.js
+++ desafio-2/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <header>
+      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 className="my-0 mr-md-auto font-weight-normal">Bazar Bartiano</h5>
+          <nav className="my-2 my-md-0 mr-md-3">
+            <a className="p-2 text-dark" href="#">Home</a>
+            <a className="p-2 text-dark" href="#">Productos</a>
+            <a className="p-2 text-dark" href="#">Ecommerce</a>
+            <a className="p-2 text-dark" href="#">Nosotros</a>
+          </nav>
+              <a class="btn btn-outline-primary" href="#">Ingresar</a>
+      </div>
+    </header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +29,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
